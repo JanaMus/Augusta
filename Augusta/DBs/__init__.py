893cf4b6691bf56833e4004594c94ae516ccd103
Jana Musilova <56636840+JanaMus@users.noreply.github.com>
2022-09-01 17:46:05 +0200

    Update __init__.py

--- Augusta/DBs/__init__.py
+++ Augusta/DBs/__init__.py
@@ -1,5 +1,1 @@
 # __init__.py
-from .get_synonyms import organism_synonyms, gene_synonyms
-from .DBs_search import *
-from .DBs_filter import filter_interactions, fill_GRN, DBsInteractions_to_GRN
-from .CC_search_filter import CC_search, CC_filter
