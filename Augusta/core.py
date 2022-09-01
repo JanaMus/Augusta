581e8e180a99dfcaabdcb3310708c6055e7c5dfc
Jana Musilova <56636840+JanaMus@users.noreply.github.com>
2022-09-01 17:44:46 +0200

    Update core.py

--- Augusta/core.py
+++ Augusta/core.py
@@ -2,24 +2,24 @@ import pandas as pd
 import numpy as np
 import sys, os
 
-from data_import import import_CountTable, genbank_process, import_GRN
-import GRN_MI
-from RNASeq_normalization import normalize_CountTable
-from motif_discovery import find_motifs
-import DBs
-from SBML_generation import GRN_to_SBML
-from data_export import export_interactions, export_GRN
+from .data_import import import_CountTable, genbank_process, import_GRN
+from .GRN_MI import GRNmi_infer, preprocessing
+from .RNASeq_normalization import normalize_CountTable
+from .motif_discovery import find_motifs
+from .DBs import DBs_search, DBs_filter, CC_search_filter, get_synonyms
+from .SBML_generation import GRN_to_SBML
+from .data_export import export_interactions, export_GRN
 
 
 # infer GRN from count table using mutual information
 def RNASeq_to_GRNmi(count_table):
     print('Mutual information computation...')
     gene_names = count_table.index
-    count_table_differences, highest_difference = GRN_MI.preprocess(count_table)
-    MI_matrix = GRN_MI.calc_MI(count_table, highest_difference)
+    count_table_differences, highest_difference = GRNmi_infer.preprocess(count_table)
+    MI_matrix = GRNmi_infer.calc_MI(count_table, highest_difference)
     CoeN_np = np.where(MI_matrix > 0, 1, MI_matrix).astype(int)
     CoeN = pd.DataFrame(CoeN_np, columns=gene_names, index=gene_names)
-    GRNmi = GRN_MI.find_interaction_type(CoeN, count_table_differences)
+    GRNmi = preprocessing.find_interaction_type(CoeN, count_table_differences)
     return GRNmi, count_table_differences
 
 # verify GRN using motif search
@@ -38,49 +38,49 @@ def GRNmi_to_GRNmotifs(GRNmi, gene_promoters, count_table_differences):
             print('No motifs reversely searched in genome. Discovered motifs have been saved as "discovered_motifs.sto".')
             return GRNmi
         else:
-            GRNmotifs = GRN_MI.find_interaction_type(CoeNmotifs, count_table_differences)
+            GRNmotifs = preprocessing.find_interaction_type(CoeNmotifs, count_table_differences)
             return GRNmotifs
 
 # search genes interactions in databases
 def get_DBs_data(organism, genes_IDs_gb, taxon, GRN = None):
     global organism_all_names
     print('Synonym organism names search...')
-    organism_all_names = DBs.organism_synonyms(organism)
+    organism_all_names = get_synonyms.organism_synonyms(organism)
     print('Synonym genes names search...')
     sys.stdout = open(os.devnull, 'w')  # block print
-    genes_IDs_all = DBs.gene_synonyms(genes_IDs_gb, taxon)
+    genes_IDs_all = get_synonyms.gene_synonyms(genes_IDs_gb, taxon)
     sys.stdout = sys.__stdout__  # enable print
     print('Synonym names search done.')
 
     # search interaction databases
-    op_interactions = DBs.search_OmniPath(taxon)
-    signor_interactions = DBs.search_Signor(taxon)
-    sl_interactions = DBs.search_SignaLink(taxon)
-    trrust_interactions = DBs.search_TRRUST(organism_all_names)
+    op_interactions = DBs_search.search_OmniPath(taxon)
+    signor_interactions = DBs_search.search_Signor(taxon)
+    sl_interactions = DBs_search.search_SignaLink(taxon)
+    trrust_interactions = DBs_search.search_TRRUST(organism_all_names)
     try:
         allDBs_interactions = pd.concat([signor_interactions, op_interactions, sl_interactions, trrust_interactions])
     except ValueError:
         print('No data searched in interaction databases.')
         return GRN
     else:
-        interactions_all, interactions_uncertain, interactions_filtered = DBs.filter_interactions(allDBs_interactions, genes_IDs_all)
+        interactions_all, interactions_uncertain, interactions_filtered = DBs_filter.filter_interactions(allDBs_interactions, genes_IDs_all)
         export_interactions(interactions_all, interactions_uncertain)
         if len(interactions_filtered) > 0:
             print('%s interactions found in databases (%s genes in GRN).' % (len(interactions_filtered), len(genes_IDs_all)))
             if GRN is None: # GRN only from databases interactions data - GRN was not inferred / imported
-                GRNdb = DBs.DBsInteractions_to_GRN(interactions_filtered)
+                GRNdb = DBs_filter.DBsInteractions_to_GRN(interactions_filtered)
             else:
                 gene_names = GRN.index
-                GRNdb = DBs.fill_GRN(GRN, gene_names, interactions_filtered) # add databases interactions to inferred / imported GRN
+                GRNdb = DBs_filter.fill_GRN(GRN, gene_names, interactions_filtered) # add databases interactions to inferred / imported GRN
         else: GRNdb = GRN # no interactions found, input GRN is returned
     export_GRN(GRNdb)
     return GRNdb
 
 # search Cell Collective database for interactions and logical rules
 def get_CC_data(genes_IDs, GRN):
-    CC_models = DBs.CC_search(organism_all_names)
+    CC_models = CC_search_filter.CC_search(organism_all_names)
     if len(CC_models) > 0:
-        GRNcc, CC_conditions = DBs.CC_filter(genes_IDs, GRN, CC_models)
+        GRNcc, CC_conditions = CC_search_filter.CC_filter(genes_IDs, GRN, CC_models)
     else:
         GRNcc = GRN.copy(); CC_conditions = {}
     return GRNcc, CC_conditions
