b43f8dc147e3c0fc26387c2360353040ae3c1f22
Jana Musilova <56636840+JanaMus@users.noreply.github.com>
2022-09-07 17:49:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta"],
-    python_requires='>=3, <=3.8',
+    python_requires='>=3, <3.9',
     install_requires=read_requirements('requirements.txt'),
 )
