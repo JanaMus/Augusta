9d42331d9a093ed6c9286875c5557bfba24aa164
Jana Musilova <56636840+JanaMus@users.noreply.github.com>
2022-09-11 18:20:16 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Jana Musilova, Zdenek Vafek, Karel Sedlar'
 author = 'Jana Musilova, Zdenek Vafek, Karel Sedlar'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.2'
+release = '1.0.3'
 
 
 # -- General configuration ---------------------------------------------------
