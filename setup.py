4cc0bd1de1e9458e8c347cc80ccf8b14ec36114f
Jana Musilova <56636840+JanaMus@users.noreply.github.com>
2025-05-12 16:09:58 +0200

    setup.py: Update to version 1.0.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_requirements(fname):
 
 setup(
     name='Augusta',
-    version='1.0.6',
+    version='1.0.7',
     packages=find_packages(),
     url='https://github.com/JanaMus/Augusta',
     license='MIT',
